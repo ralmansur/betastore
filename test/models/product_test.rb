b4b13ebb5abbc16c0fe07d4b4d436ca79273420f
Rafee Al-Mansur <ralmansur@gmail.com>
2014-10-16 07:17:55 +0800

    Updated Product_test to test validations and if name is present.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,15 +2,14 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   test "create a product" do
-    product = Product.new
+    product = Product.new(name: "Application", price: 0.99)
     assert product.save, product.errors.full_messages
   end
 
   test "name is required" do
-    product = Product.new
+    product = Product.new(price: 0.99)
     refute product.save, 'name should be required'
     assert product.errors.full_messages.include?("Name can't be blank")
   end
-
 
 end
