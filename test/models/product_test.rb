54d3e28b239db27a8107bc7e620e36ddd29a9bab
Rafee Al-Mansur <ralmansur@gmail.com>
2014-10-16 08:23:04 +0800

    Added custom error message to name and price validation.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,13 +9,13 @@ class ProductTest < ActiveSupport::TestCase
   test "name is required" do
     product = Product.new(price: 0.99)
     refute product.save, 'name should be required'
-    assert product.errors.full_messages.include?("Name can't be blank")
+    assert product.errors.full_messages.include?("Name is required"), product.errors.full_messages
   end
 
   test "price must be number" do
     product = Product.new(name: "Application", price: "$0.99")
     refute product.save, 'price must be a number'
-    assert product.errors.full_messages.include?("Price is not a number")
+    assert product.errors.full_messages.include?("Price isn't a number"), product.errors.full_messages
   end
 
 end
